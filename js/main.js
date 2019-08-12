e17530dcbe23c92b5788de0b3edf3d17a30f2ef8
morfiy <edurnev@keaz.ru>
2019-08-12 10:39:36 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -30,3 +30,19 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#button-up').fadeIn();
+        } else {
+            $('#button-up').fadeOut();
+        }
+    });
+    $('#button-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
